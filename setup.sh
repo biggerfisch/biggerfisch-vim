c642c5e9567f6ab89e3c4ec238e484d020e6aac4
Avery Fischer <avery@averyjfischer.com>
2014-10-27 19:06:46 -0400

    Spelling fixes and less confusing errors

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ NC='\e[0m'
 
 function error_exit
 {
-	echo -e "${red}ERROR:${NC}"
-	echo $1
+	echo -e "${red}ERROR${NC}"
+	echo "QUITTING"
 	exit 1;
 }
 
-echo -e "${green}Install pathogen..."
-./install_pathogen.sh > /dev/null || error_exit "${red}Could not install pathogen!${NC}"
+echo -e "${green}Installing pathogen..."
+./install_pathogen.sh > /dev/null || error_exit "Could not install pathogen!"
 echo -e "${green}Retrieving plugins...${NC}"
-git submodule update --init > /dev/null || error_exit "${red}Failed to get plugins!${NC}"
+git submodule update --init > /dev/null || error_exit "Failed to get plugins!"
 echo -e "${green}Linking .vimrc to home directory...${NC}"
-./link_rc.sh || error_exit "${red}Failed to setup .vimrc${NC}"
+./link_rc.sh || error_exit "Failed to setup .vimrc"
 echo -e "${green}Done!${NC}"
