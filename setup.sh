780441ccdf1bb3c76e3927d340ebfb04c18bb193
Avery Fischer <avery@averyjfischer.com>
2014-10-27 19:00:05 -0400

    Colors

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m'
+
 function error_exit
 {
-	echo "ERROR:"
+	echo -e "${red}ERROR:${NC}"
 	echo $1
 	exit 1;
 }
 
-echo "Install pathogen..."
-./install_pathogen.sh > /dev/null || error_exit "Could not install pathogen!"
-echo "Retrieving plugins..."
-git submodule update --init > /dev/null || error_exit "Failed to get plugins!"
-echo "Linking .vimrc to home directory"
-./link_rc.sh || error_exit "Failed to setup .vimrc"
-echo "Done!"
+echo -e "${green}Install pathogen..."
+./install_pathogen.sh > /dev/null || error_exit "${red}Could not install pathogen!${NC}"
+echo -e "${green}Retrieving plugins...${NC}"
+git submodule update --init > /dev/null || error_exit "${red}Failed to get plugins!${NC}"
+echo -e "${green}Linking .vimrc to home directory...${NC}"
+./link_rc.sh || error_exit "${red}Failed to setup .vimrc${NC}"
+echo -e "${green}Done!${NC}"
