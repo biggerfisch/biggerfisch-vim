240c707d0b7b9e1bc68f5c6c63c16d14d96cf127
Avery Fischer <avery@averyjfischer.com>
2014-11-02 10:42:24 -0500

    Script to update repo and plugins

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#This script updates all the vim plugins 
+
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m'
+
+function error_exit
+{
+	echo -e "${red}ERROR${NC}"
+	echo "$1"
+	echo "QUITTING"
+	exit 1;
+}
+
+git pull || error_exit 'Could not update main repo'
+
+git submodule foreach git checkout master || error_exit 'Could not checkout master on one or more plugins!'
+git submodule foreach git pull || error_exit 'Could not update one or more plugins'
+echo -e "${green}Done!${NC}"
