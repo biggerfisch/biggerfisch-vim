5dcbbca913004d3c8f72672629459e5a33b8424d
Avery Fischer <avery_fischer@apple.com>
2015-04-12 21:05:52 -0700

    No more forced merges on pull...

--- update.sh
+++ update.sh
@@ -14,9 +14,9 @@ function error_exit
 	exit 1;
 }
 
-git pull || error_exit 'Could not update main repo'
+git pull 
 
 git submodule init || error_exit 'Could not init submodules'
 git submodule foreach git checkout master || error_exit 'Could not checkout master on one or more plugins!'
-git submodule foreach git pull || error_exit 'Could not update one or more plugins'
+git submodule foreach git pull --ff || error_exit 'Could not update one or more plugins'
 echo -e "${green}Done!${NC}"
